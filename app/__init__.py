b1205ad8baa586847a7f0464fb9ec3a7688f1e6f
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-01-18 00:10:54 +0300

    uncomment to trace circular import error

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 from instance.config import app_config
 
-# from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager
 # this is where the error originates from 
 
 
 db = SQLAlchemy()
-# jwt = JWTManager()
+jwt = JWTManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
-    # jwt.init_app(app)
+    jwt.init_app(app)
 
 
     from app.apis import apiv1_bp
