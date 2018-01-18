9065ff92f583f87e6905e9af2b17367de84891f1
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-01-18 12:19:27 +0300

    Initial test for fixing travis failures

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from instance.config import app_config
 
-from flask_jwt_extended import JWTManager
-# this is where the error originates from 
-
+from flask_jwt_extended import import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity
+)
 
 db = SQLAlchemy()
 jwt = JWTManager()
